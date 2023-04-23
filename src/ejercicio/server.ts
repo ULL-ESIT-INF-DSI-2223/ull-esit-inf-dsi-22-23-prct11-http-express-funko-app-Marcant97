e788fef0d1736f14425f98e22d58c3e228aee730
Marco Barroso <alu0101386560@ull.edu.es>
2023-04-23 11:20:53 +0000

    código asíncorno con promesas

--- src/ejercicio/server.ts
+++ src/ejercicio/server.ts
@@ -1,24 +1,18 @@
-import net from 'net';
 import { addFunko, eliminarFunko, listaFunkos, mostrarFunko} from './funciones.js';
 import { Funko } from './funko.js';
 import { Tipo, Genero } from './types.js';
-
 import express from 'express';
-import { appendFile } from 'fs';
 
-// Toda función que defina y que ejecute código asíncrono deberá implementarse haciendo uso del patrón callback. 
-// Lo anterior permitirá, entre otras cosas, establecer las pruebas de un modo más sencillo.
 
 const server = express();
 
-// get: Para obtener información sobre un Funko Pop concreto de un usuario o para listar todos sus Funko Pops.
-server.get('/funkos', (req, res) => {
+server.get('/funkos', async (req, res) => {
   // se recibe sólo el nombre del usuario -> lista de funkos
   // se recibe el nombre del usuario y el id del funko -> funko concreto
   if (req.query.usuario) {
     if (req.query.id) {
       // se recibe un array con un funko, lo guardamos y lo enviamos en formato json
-      const funkos = mostrarFunko(req.query.usuario as string, Number(req.query.id) as number);
+      const funkos = await mostrarFunko(req.query.usuario as string, Number(req.query.id) as number);
       if (funkos.length === 0) {
         res.status(404).send("No existe el funko solicitado");
       }
@@ -29,14 +23,13 @@ server.get('/funkos', (req, res) => {
     }
     else {
       // se recibe un array con un funko, lo guardamos y lo enviamos en formato json
-      const funkos = listaFunkos(req.query.usuario as string);
+      const funkos = await listaFunkos(req.query.usuario as string);
       if (funkos.length === 0) {
         res.status(404).send("No hay funkos en la colección");
       }
       else {
         res.send(funkos);
       }
-
     }
   }
   else {
@@ -44,64 +37,54 @@ server.get('/funkos', (req, res) => {
   }
 });
 
-
-
-server.delete('/funkos', (req, res) => {
+server.delete('/funkos', async (req, res) => {
   if (req.query.usuario && req.query.id) {
-    const resultado = eliminarFunko(req.query.usuario as string, Number(req.query.id) as number);
+    const resultado = await eliminarFunko(req.query.usuario as string, Number(req.query.id) as number);
     if (resultado === true) {
       res.send("Funko eliminado");
-    }
-    else {
+    } else {
       res.status(404).send("No se ha encontrado el funko");
     }
   }
 });
 
 
+server.post('/funkos', async (req, res) => {
+  const funko = new Funko(req.query.nombre as string, req.query.descripcion as string, req.query.tipo as Tipo, req.query.genero as Genero, req.query.franquicia as string, Number(req.query.numero) as number, Boolean(req.query.exclusivo) as boolean, req.query.caracteristicasEspeciales as string, Number(req.query.valorMercado) as number, Number(req.query.id) as number);
+  const resultado = await addFunko(funko, req.query.usuario as string);
 
-
-// post: Para añadir un Funko Pop a la lista de un usuario.
-server.post('/funkos', (req, res) => {
-  if (req.query.usuario && req.query.nombre && req.query.descripcion && req.query.tipo && req.query.genero && req.query.franquicia && req.query.numero && req.query.exclusivo && req.query.caracteristicasEspeciales && req.query.valorMercado) {
-    const funko = new Funko(req.query.nombre as string, req.query.descripcion as string, req.query.tipo as Tipo, req.query.genero as Genero, req.query.franquicia as string, Number(req.query.numero) as number, Boolean(req.query.exclusivo) as boolean, req.query.caracteristicasEspeciales as string, Number(req.query.valorMercado) as number, Number(req.query.id) as number);
-    const resultado = addFunko(funko, req.query.usuario as string);
-    if (resultado === true) {
-      res.send("Funko añadido a la colección correctamente");
-    }
-    else {
-      res.status(404).send("No se ha podido añadir el funko");
-    }
-  }
-  else {
-    res.status(400).send("No se han recibido todos los datos");
+  if (resultado) {
+    res.send("Funko añadido");
+  } else {
+    res.status(400).send("Funko no añadido");
   }
 });
 
-// patch: Para modificar la información de un Funko Pop existente en la lista de un usuario.
-server.patch('/funkos', (req, res) => {
-  if (req.query.usuario && req.query.nombre && req.query.descripcion && req.query.tipo && req.query.genero && req.query.franquicia && req.query.numero && req.query.exclusivo && req.query.caracteristicasEspeciales && req.query.valorMercado) {
-    // eliminar funko
-    const resultado1 = eliminarFunko(req.query.usuario as string, Number(req.query.id) as number);
-    if (resultado1 === true) {
-      // añadir funko
-      const funko = new Funko(req.query.nombre as string, req.query.descripcion as string, req.query.tipo as Tipo, req.query.genero as Genero, req.query.franquicia as string, Number(req.query.numero) as number, Boolean(req.query.exclusivo) as boolean, req.query.caracteristicasEspeciales as string, Number(req.query.valorMercado) as number, Number(req.query.id) as number);
-      const resultado2 = addFunko(funko, req.query.usuario as string);
-      if (resultado2 === true) {
-        res.send("Funko modificado correctamente");
+server.patch('/funkos', async (req, res) => {
+  try {
+    if (req.query.usuario && req.query.nombre && req.query.descripcion && req.query.tipo && req.query.genero && req.query.franquicia && req.query.numero && req.query.exclusivo && req.query.caracteristicasEspeciales && req.query.valorMercado) {
+      // primero eliminamos el funko
+      const resultado1 = await eliminarFunko(req.query.usuario as string, Number(req.query.id) as number);
+      if (resultado1 === true) {
+        // luego añadimos el funko
+        const funko = new Funko(req.query.nombre as string, req.query.descripcion as string, req.query.tipo as Tipo, req.query.genero as Genero, req.query.franquicia as string, Number(req.query.numero) as number, Boolean(req.query.exclusivo) as boolean, req.query.caracteristicasEspeciales as string, Number(req.query.valorMercado) as number, Number(req.query.id) as number);
+        const resultado2 = await addFunko(funko, req.query.usuario as string);
+        if (resultado2 === true) {
+          res.send("Funko modificado correctamente");
+        } else {
+          res.status(404).send("No se ha podido modificar el funko");
+        }
+      } else {
+        res.status(404).send("No se ha encontrado el funko a modificar");
       }
-      else {
-        res.status(404).send("No se ha podido modificar el funko");
-      }
+    } else {
+      res.status(400).send("Faltan parámetros en la petición");
     }
-    else {
-      res.status(404).send("No se ha encontrado el funko a modificar");
-    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ha ocurrido un error en el servidor");
   }
-
-
 });
-
 
 server.listen(3000, () => {
   console.log('Server is up on port 3000');
